8563f423b65b0c9ca07927a2e7ecc250d1ec49b0
josephl17 <joseph.lauria17@gmail.com>
2024-06-20 16:34:53 -0500

    functional guessing game

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-console.log("HELLO PAPA PLATOON!")
+console.log("HELLO X-RAY PLATOON!")
 // Your function(s) should go here that will interact with the webpage or DOM
+
+// declares a variable and set it to random number between 1 and 100
+let randomNumber = Math.floor(Math.random() * 100);
+
+// function 
+const guessingGame = (num) => {
+    console.log(num)
+        if (num === randomNumber) {
+            document.getElementById('h1').innerText = "Hooray, you won!"
+            solved = true
+        }
+        else if (num > randomNumber) {
+            document.getElementById('h1').innerText = "Your guess is too high"
+        }
+        else if (num < randomNumber) {
+            document.getElementById('h1').innerText = "Your guess is too low"
+        }
+    // }
+}
+
+document.getElementById('button').onclick = () => {
+    let guessNum = Number(document.getElementById('input').value);
+    guessingGame(guessNum)
+    document.getElementById('numbers').innerText += guessNum + "\n"
+}
